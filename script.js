fbc2eaf64f92363e0442eaeb8f3c093dcb90ae03
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-04-05 11:39:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+function manipulateArray() {
+	return new Promise(resolve => {
+		setTimeout(() =>{
+			resolve([1,2,3,4]);		
+			},3000);
+	})
+	.then(array =>{
+		return new Promise(resolve => {
+		const filteredArray=array.filter(num => num % 2 === 0);
+		const output=document.getElementById('output');
+		output.textContent=JSON.stringify(filteredArray);
+		setTimeout(()=>{
+			resolve(filteredArray);
+		},1000)
+	})
+})
+.then( filteredArray =>{
+	return new Promise(resolve =>{
+		const multipliedArray=filteredArray.map(num => num * 2);
+		const output=document.getElementById('output');
+		setTimeout(()=>{
+			output.textContent=JSON.stringify(multipliedArray); 
+			resolve(multipliedArray);
+		},2000)
+	})
+})
+}
